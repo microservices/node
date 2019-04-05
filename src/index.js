d49845a09e8f587cf3ff94e4d2a67e4e437ca804
Sebastian Wilzbach <seb@wilzba.ch>
2019-04-05 17:14:34 +0200

    Initialize Node OMG template

---
+++ src/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const Koa = require('koa');
+const router = require('koa-router')();
+const body = require('koa-json-body');
+
+const app = new Koa();
+const port = 8080;
+
+router.post('/message', (ctx, next) => {
+  const { name } = ctx.request.body;
+  ctx.body = {message: `Hello ${name}`};
+});
+
+app.use(body())
+app.use(router.routes());
+app.listen(port);
+console.log(`Listening on localhost:${port}`);
